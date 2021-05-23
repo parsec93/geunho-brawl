1e25facc91be99934caface097b1d853692b87e9
geunho <rnjs8657@naver.com>
2021-05-23 18:48:35 +0900

    첫 커밋

---
+++ src/main/java/com/geunho/geunhogradle/GeunhoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.geunho.geunhogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeunhoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeunhoGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geunho/geunhogradle/GeunhoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geunho.geunhogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeunhoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
